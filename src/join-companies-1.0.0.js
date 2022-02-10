53588e425ba143ec3435a4559a21af07bbd40c02
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2022-02-10 20:39:40 +0800

    Update script to use the flags utility function

--- src/join-companies-1.0.0.js
+++ src/join-companies-1.0.0.js
@@ -1,9 +1,25 @@
+import { booleanOption, getOptions, parseFlags, printHelp, useFlags } from './utils/flags'
 import { names } from './utils/companies'
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    ns.tail()
-    for (const name of names) {
-        ns.applyToCompany(name, 'business')
+    await useFlags(ns, getFlags, async (flags) => {
+        for (const name of names) {
+            ns.applyToCompany(name, 'business')
+        }
+    })
+}
+
+/**
+ * @param {NS} ns 
+ */
+function getFlags(ns) {
+    const options = getOptions([])
+    function processFlags(ns, flags) {
+        return {}
     }
+    function help(ns, options) {
+        printHelp(ns, "This script will manage Sleeves.", options)
+    }
+    return parseFlags(ns, options, processFlags, help)
 }
