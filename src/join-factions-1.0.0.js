6149982047b1f2af3cccec5af2fcbc29bc5e91a8
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2022-02-10 20:37:46 +0800

    Update script to use the flags util

--- src/join-factions-1.0.0.js
+++ src/join-factions-1.0.0.js
@@ -1,18 +1,32 @@
+import { booleanOption, getOptions, parseFlags, printHelp, useFlags } from './utils/flags'
 import { cityFactions } from './utils/factions'
 
-/** 
- * @param {NS} ns
- **/
+/** @param {NS} ns **/
 export async function main(ns) {
-    ns.tail()
-    while (true) {
-        const invitations = ns.checkFactionInvitations()
-        for (const invite of invitations) {
-            if (cityFactions.includes(invite)) {
-                continue
+    await useFlags(ns, getFlags, async (flags) => {
+        while (true) {
+            const invitations = ns.checkFactionInvitations()
+            for (const invite of invitations) {
+                if (cityFactions.includes(invite)) {
+                    continue
+                }
+                ns.joinFaction(invite)
             }
-            ns.joinFaction(invite)
+            await ns.sleep(1000);
         }
-        await ns.sleep(1000);
+    })
+}
+
+/**
+ * @param {NS} ns 
+ */
+function getFlags(ns) {
+    const options = getOptions([])
+    function processFlags(ns, flags) {
+        return {}
     }
+    function help(ns, options) {
+        printHelp(ns, "This script will automatically join non-city Factions.", options)
+    }
+    return parseFlags(ns, options, processFlags, help)
 }
